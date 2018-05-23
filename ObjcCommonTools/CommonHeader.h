b479b7488c51d9667999fd696c1d9afb6a95f331
JK-King <zhang_jiankun521@163.com>
2018-05-23 15:56:27 +0800

    添加常量头文件和库详细说明

---
+++ ObjcCommonTools/CommonHeader.h
@@ -0,0 +1,13 @@
+//
+//  CommonHeader.h
+//  Demo
+//
+//  Created by Jiankun Zhang on 2018/5/23.
+//  Copyright © 2018年 Jiankun Zhang. All rights reserved.
+//
+
+#ifndef CommonHeader_h
+#define CommonHeader_h
+
+
+#endif /* CommonHeader_h */
